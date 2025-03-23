d4d958157c1d8c86e5f250ce36eb63231edf299e
fehervaryy <martindmhe@gmail.com>
2025-03-22 23:54:31 -0400

    add images

--- src/app/_about/page.tsx
+++ src/app/_about/page.tsx
@@ -3,21 +3,24 @@ import Image from 'next/image';
 import AboutTrees from '@/assets/about-trees-1.svg';
 import AboutGroup from '@/assets/about-group-1.svg';
 import AboutGroup2 from '@/assets/about-group-2.svg';
+import AboutImage1 from '@/assets/about_image_1.svg';
+import AboutImage2 from '@/assets/about_image_2.svg';
+
 
 export default function About() {
   return (
-    <section className="bg-[#cfedaf] relative">
-      <Image src={AboutTrees} alt="Trees" className="absolute top-[280px] left-0 z-0" />
-      <Image src={AboutGroup} alt="Group" className="absolute top-[600px] right-0 z-0" />
-      <Image src={AboutGroup2} alt="Group" className="absolute top-[400px] right-0 z-0" />
+    <section className="bg-[#cfedaf] relative overflow-x-hidden w-full">
+        <Image src={AboutTrees} alt="Trees" className="absolute top-[280px] left-0 z-0" />
+        <Image src={AboutGroup} alt="Group" className="absolute top-[550px] right-0 z-0" />
+        <Image src={AboutGroup2} alt="Group" className="absolute top-[350px] right-0 z-0" />
       {/* <Text textType="heading-lg" as="h2">
         About
       </Text> */}
       
       {/* "main" content */}
       <div className="z-20 flex flex-col p-40 relative">
-        <div className="flex flex-col gap-4 max-w-[600px]">
-          <Text textType="heading-lg" textWeight="bold">
+        <div className="flex flex-col gap-4 max-w-[760px]">
+          <Text textType="heading-lg" textWeight="bold" className="max-w-[600px]">
             Join us for an unforgettable weekend
           </Text>
           <Text textType="paragraph-lg">
