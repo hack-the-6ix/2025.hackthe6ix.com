deb3ac98d31384d0efcfe31d43bd9e319127bbf4
jeffrey-zang <jeffrey.zang.07@gmail.com>
2025-04-09 22:28:28 -0400

    update

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+// next.config.ts
+import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
+
+initOpenNextCloudflareForDev();
